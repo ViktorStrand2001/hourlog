9215c3002ba4bc52e74afb6799334f821b664ff3
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2024-12-06 12:44:41 +0100

    New project HourLog is a web application where employees can submit there work hours and task submission.
    Added:
    Shadcn for modern components.
    MaxWidthWrapper for the max width for the application.

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+
+export default function Home() {
+
+    
+
+    return (
+        <div>
+            <MaxWidthWrapper>
+                <div>hej</div>
+            </MaxWidthWrapper>
+        </div>
+    )
+}
---
+++ src/components/MobileViewCaldender.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MobileViewCaldender() {
+  return (
+    <div>MobileViewCaldender</div>
+  )
+}
+
+
